39b1b782522d861c8279e67a6ecfbacd532425bc
Shynggys Umbetov <shynggys@web-premiere.fr>
2021-06-25 13:37:08 +0200

    refactor

--- Components/Search.js
+++ Components/Search.js
@@ -1,13 +1,28 @@
 // Components/Search.js
 import React from 'react'
+import { View, TextInput, StyleSheet, Button } from 'react-native'
 
-class Search extends React.Component {
+export default class Search extends React.Component {
     render() {
         return (
-            <View>
-                <TextInput placeholder='Titre du film'/>
+            <View style={{ marginTop: 20 }}>
+                <TextInput
+                    style={styles.textinput}
+                    placeholder='Titre du film'
+                />
                 <Button title='Rechercher' onPress={() => {}}/>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    textinput: {
+        marginLeft: 5,
+        marginRight: 5,
+        height: 50,
+        borderColor: '#000000',
+        borderWidth: 1,
+        paddingLeft: 5
+    }
+})
