b6e49673ea6ec2c055673313a5e3bcb49cf2308d
Shynggys Umbetov <shynggys@web-premiere.fr>
2021-06-25 13:16:44 +0200

    First commit and Search component

---
+++ Components/Search.js
@@ -0,0 +1,13 @@
+// Components/Search.js
+import React from 'react'
+
+class Search extends React.Component {
+    render() {
+        return (
+            <View>
+                <TextInput placeholder='Titre du film'/>
+                <Button title='Rechercher' onPress={() => {}}/>
+            </View>
+        )
+    }
+}
