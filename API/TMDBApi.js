f65dfb88078f9d5037979efcf10087578ea12ad1
Shynggys Umbetov <shynggys@web-premiere.fr>
2021-06-28 15:58:44 +0200

    Get data from api of TMDB

---
+++ API/TMDBApi.js
@@ -0,0 +1,8 @@
+const API_TOKEN = "8189dfa21c80444121209957e89f4579";
+
+export function getFilmsFromApiWithSearchedText (text) {
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text
+    return fetch(url)
+        .then((response) => response.json())
+        .catch((error) => console.error(error))
+}
--- Components/Search.js
+++ Components/Search.js
@@ -1,17 +1,28 @@
 import React from 'react'
-import { StyleSheet, View, TextInput, Button, Text, FlatList } from 'react-native'
-import { films } from '../Helpers/filmsData.js'
+import { StyleSheet, View, TextInput, Button, FlatList } from 'react-native'
+// import { films } from '../Helpers/filmsData.js'
 import FilmItem from './FilmItem'
+import { getFilmsFromApiWithSearchedText } from "../API/TMDBApi";
 
 class Search extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { films: []}
+    }
+
+    _loadFilms() {
+        getFilmsFromApiWithSearchedText("star").then(data => this.setState({films: data.results }))
+    }
+
     render() {
         return (
             <View style={styles.main_container}>
                 <TextInput style={styles.textinput} placeholder='Titre du film'/>
-                <Button title='Rechercher' onPress={() => {}}/>
+                <Button title='Rechercher' onPress={() => this._loadFilms()}/>
                 {/* Ici j'ai simplement repris l'exemple sur la documentation de la FlatList */}
                 <FlatList
-                    data={films}
+                    data={this.state.films}
                     keyExtractor={(item) => item.id.toString()}
                     renderItem={({item}) => <FilmItem film={item}/>}
                 />
