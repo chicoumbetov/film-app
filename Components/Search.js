b89311ff25c35b889a85dc2ff869c3416d725b37
Shynggys Umbetov <shynggys@web-premiere.fr>
2021-06-30 14:10:49 +0200

    Pagination

--- Components/Search.js
+++ Components/Search.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, View, TextInput, Button, FlatList } from 'react-native'
+import {StyleSheet, View, TextInput, Button, FlatList, ActivityIndicator} from 'react-native'
 // import { films } from '../Helpers/filmsData.js'
 import FilmItem from './FilmItem'
 import { getFilmsFromApiWithSearchedText } from "../API/TMDBApi";
@@ -8,8 +8,12 @@ class Search extends React.Component {
 
     constructor(props) {
         super(props);
+        this.page = 0
+        this.totalPages = 0
+
         this.state = {
             films: [],
+            isLoading: false
         }
         // Initialisation de notre donnée searchedText en dehors du state
         this.searchedText = ""
@@ -17,9 +21,33 @@ class Search extends React.Component {
 
     // Seulement si le texte recherché n'est pas vide
     _loadFilms() {
+        this.setState({isLoading: true})
         if (this.searchedText.length > 0) {
-            getFilmsFromApiWithSearchedText(this.searchedText)
-                .then(data => this.setState({films: data.results }))
+            // +1 to call next page, otherwise we'll call always same page
+            getFilmsFromApiWithSearchedText(this.searchedText, this.page+1)
+                .then(data => {
+                    this.page = data.page
+                    this.totalPages = data.total_pages
+                    this.setState({
+                        // 1 copie of films array already taken
+                        // 1 copie of new film lists array taken when searched
+                        // then we put them in one array concatenanted
+                        films: [ ...this.state.films, ...data.results], // ES6
+                        // old version : films: this.state.films.concat(data.results),
+                        isLoading: false
+                    })
+                    }
+                )
+        }
+    }
+
+    _displayLoading() {
+        if (this.state.isLoading) {
+            return (
+                <View style={styles.loading_container}>
+                    <ActivityIndicator size="large"/>
+                </View>
+            )
         }
     }
 
@@ -29,22 +57,54 @@ class Search extends React.Component {
         this.searchedText = text
     }
 
+    // pour différencier une recherche de films de l'appel d'une nouvelle page
+    _searchFilms() {
+        // on remettra à 0 notre state
+        this.page = 0
+        this.totalPages = 0
+        this.setState({
+            // plus spécifiquement, on remettra à zéro les films de notre state, juste avant d'appeler l'API
+            films: []
+        },
+             //setState  possède un paramètre  callback  qui permet d'exécuter une action dès que notre state a fini de se mettre à jour
+            // d'abord remettre le state et mes films à zéro, puis lancer une nouvelle recherche
+            () => {
+                // J'utilise la paramètre length sur mon tableau de films pour vérifier qu'il y a bien 0 film
+                console.log("Page : " + this.page + " / TotalPages : " + this.totalPages + " / Nombre de films : " + this.state.films.length)
+                this._loadFilms()
+            }
+            )
+
+    }
+
     render() {
         return (
             <View style={styles.main_container}>
                 <TextInput
-                    onSubmitEditing={() => this._loadFilms()}
+                    placeholder='Titre du film'
                     onChangeText={(text) => this._searchTextInputChanged(text)}
                     style={styles.textinput}
-                    placeholder='Titre du film'
+                    // also possible to validate by pressing 'enter'
+                    // as well as clicking on button
+                    onSubmitEditing={() => this._searchFilms()}
                 />
-                <Button title='Rechercher' onPress={() => this._loadFilms()}/>
+                <Button title='Rechercher' onPress={() => this._searchFilms()}/>
                 {/* Ici j'ai simplement repris l'exemple sur la documentation de la FlatList */}
                 <FlatList
                     data={this.state.films}
                     keyExtractor={(item) => item.id.toString()}
                     renderItem={({item}) => <FilmItem film={item}/>}
+                    // detection half screen before the end of list
+                    onEndReachedThreshold={0.5}
+                    onEndReached={() => {
+                        // console.log("onEndReached")
+                        if (this.page < this.totalPages) {
+                            this._loadFilms()
+                        }
+                    }
+                    }
                 />
+                {this._displayLoading()}
             </View>
         )
     }
@@ -64,5 +124,14 @@ const styles = StyleSheet.create({
         borderColor: '#000000',
         borderWidth: 1,
         paddingLeft: 5
+    },
+    loading_container: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 100,
+        bottom: 0,
+        alignItems: 'center',
+        justifyContent: 'center'
     }
 })
